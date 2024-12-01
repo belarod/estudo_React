b74adfc594c85cb600655ae11ebbb33365eec985
Isabela Rodrigues <bela.rodjj@gmail.com>
2024-12-01 20:00:56 -0300

    Tarefa prof.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
-function App() {
-  return <p>Limpando!</p>
+import { useState } from "react";
+
+export function App() {
+  const [tarefas, setTarefas] = useState([] as string[]);
+  const [tarefa, setTarefa] = useState("");
+
+  function deletaTarefa(indexDeletar: number) {
+    const nextTarefas = tarefas.filter(
+      (tarefa, index) => indexDeletar !== index
+    );
+    setTarefas(nextTarefas);
+  }
+
+  function adicionarTarefa(tarefa: string) {
+    setTarefas(tarefas.concat(tarefa));
+    setTarefa("");
+  }
+
+
+  
+  const tarefasList = tarefas.map((tarefa, index) => (
+    <li key={index}>
+      {tarefa}
+      <button onClick={() => deletaTarefa(index)}>X</button>
+    </li>
+  ));
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={tarefa}
+          onChange={(event) => setTarefa(event.target.value)}
+        />
+        <button onClick={() => adicionarTarefa(tarefa)}>Add</button>
+      </div>
+      <ul>{tarefasList}</ul>
+    </div>
+  );
 }
 
 export default App
-
-
